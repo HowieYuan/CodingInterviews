d29dda83edc3b3dcc44f4bed26c1b26a3a0c32f0
swit <swithaoy@gmail.com>
2018-11-21 17:16:53 +0800

    跳台阶

---
+++ src/main/java/offer/Ten2.java
@@ -0,0 +1,44 @@
+package offer;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description 青蛙跳台阶
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+ * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ * @Date 2018-11-21
+ * @Time 15:57
+ */
+public class Ten2 {
+    /**
+     * 迭代
+     */
+    public int jumpFloor2(int target) {
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        int pre = 1, result = 2, n = 2;
+        while (target != n++) {
+            result = pre + result;
+            pre = result - pre;
+        }
+        return result;
+    }
+
+    /**
+     * 递归
+     */
+    public int jumpFloor(int target) {
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        return jumpFloor(target - 1) + jumpFloor(target - 2);
+    }
+}
---
+++ src/main/java/offer/Ten3.java
@@ -0,0 +1,32 @@
+package offer;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description 青蛙变态跳台阶
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+ * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ * @Date 2018-11-21
+ * @Time 16:04
+ */
+public class Ten3 {
+    /**
+     *          | 1       ,(n=0 )
+     *
+     * f(n) =   | 1       ,(n=1 )
+     *
+     *          | 2*f(n-1),(n>=2)
+     *
+     * f(n) = f(0) + f(1) + f(2) + f(3) + ... + f(n-2) + f(n-1) = f(n-1) + f(n-1)
+     */
+    public int JumpFloor(int target) {
+        if (target <= 0) {
+            return -1;
+        } else if (target == 1) {
+            return 1;
+        } else {
+            return 2 * JumpFloor(target - 1);
+        }
+    }
+}
