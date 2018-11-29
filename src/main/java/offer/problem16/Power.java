2376072868eee6ca196d97613960adaa1e19c5ae
swit <swithaoy@gmail.com>
2018-11-29 20:24:10 +0800

    16,17

---
+++ src/main/java/offer/problem16/Power.java
@@ -0,0 +1,58 @@
+package offer.problem16;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description p110   16. 数值的整数次方
+ * @Date 2018-11-29
+ * @Time 14:21
+ */
+public class Power {
+    /**
+     * 常规方法
+     *
+     * @param base     底数
+     * @param exponent 指数
+     */
+    public static double power(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        int absEx = Math.abs(exponent);
+        double num = base;
+        while (absEx > 1) {
+            base *= num;
+            absEx--;
+        }
+        return exponent < 0 ? 1 / base : base;
+    }
+
+    /**
+     * 快速幂
+     */
+    public double Power(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        int absEx = Math.abs(exponent);
+        double result = 1;
+        while (absEx != 0) {
+            if ((absEx & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            absEx >>= 1;
+        }
+        return exponent < 0 ? 1 / result : result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, -3));
+        System.out.println(power(0, 5));
+        System.out.println(power(100, 0));
+        System.out.println(power(0, -3));
+
+        System.out.println(Math.pow(0, 0));
+    }
+}
---
+++ src/main/java/offer/problem17/ToMax.java
@@ -0,0 +1,96 @@
+package offer.problem17;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description p114  17. 打印从1到最大的n位数
+ * @Date 2018-11-29
+ * @Time 17:42
+ */
+public class ToMax {
+    /**
+     * 输入数字n，按顺序打印出从1最大的n位十进制
+     *
+     * @param n 控制数字
+     */
+    private static void printlToMaxOfDiguts1(int n) {
+        if (n <= 0) {
+            throw new RuntimeException("非法输入！");
+        }
+        // 分配空间
+        int[] number = new int[n];
+        // 从第0位开始赋值
+        printlToMaxOfDigutsRecursively(number, 0);
+    }
+
+    /**
+     * 输入数字n，按顺序打印出从1最大的n位十进制数。
+     * 递归方法
+     *
+     * @param number 给定数组
+     * @param n      当前控制索引
+     */
+    private static void printlToMaxOfDigutsRecursively(int[] number, int n) {
+        int length = 10;
+        if (n != number.length) {
+            for (int i = 0; i < length; i++) {
+                number[n] = i;
+                // 递归调用，给第n+1位赋值
+                printlToMaxOfDigutsRecursively(number, n + 1);
+            }
+        } else {
+            // 当所有空间赋值完毕执行输出
+            printNums(number);
+        }
+    }
+
+
+    /**
+     * 迭代
+     */
+    public static void printToMaxOfDigits(int n) {
+        if (n <= 0) {
+            return;
+        }
+        int[] nums = new int[n];
+        while (getNums(nums)) {
+            printNums(nums);
+        }
+    }
+
+    private static boolean getNums(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 9) {
+                if (i == 0) {
+                    return false;
+                }
+                nums[i] = 0;
+                continue;
+            }
+            nums[i]++;
+            break;
+        }
+        return true;
+    }
+
+    private static void printNums(int[] nums) {
+        int index = 0;
+        while (index < nums.length && nums[index] == 0) {
+            index++;
+        }
+        // 从第一个非0的元素开始输出
+        for (int i = index; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String[] args) {
+        // 递归调用
+//        printlToMaxOfDiguts1(2);
+        System.out.println("==========");
+        // 非递归调用
+        printlToMaxOfDiguts1(3);
+    }
+}
