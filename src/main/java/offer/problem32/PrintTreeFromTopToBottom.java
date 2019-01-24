30500666fabb197d2c9fb71eead7cd0a8a3687b5
swit <swithaoy@gmail.com>
2019-01-24 22:29:25 +0800

    32：层次遍历树

---
+++ src/main/java/offer/problem32/PrintTreeFromTopToBottom.java
@@ -0,0 +1,45 @@
+package offer.problem32;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description 32. 从上往下打印二叉树（层次遍历二叉树）
+ * @Date 2019-01-24
+ * @Time 20:49
+ */
+public class PrintTreeFromTopToBottom {
+    public ArrayList<Integer> printFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root != null) {
+            //使用队列辅助运算
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return list;
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
